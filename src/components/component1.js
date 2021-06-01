276afeba1674100914273a3a7f2412d4df61963b
sirhackalot <user123@users-MacBook-Pro.local>
2021-05-31 20:38:08 -0600

    first component

---
+++ src/components/component1.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class component1 extends Component {
+  render() {
+    return (
+      <div>
+        <div>This is the first component</div>.
+      </div>
+    )
+  }
+}
