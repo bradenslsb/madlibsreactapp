38d466dbda82b70318370a6f975a33b23cd45831
sirhackalot <user123@users-MacBook-Pro.local>
2021-06-04 20:42:25 -0600

    initial commit

--- src/components/component1.js
+++ src/components/component1.js
@@ -4,7 +4,8 @@ export default class component1 extends Component {
   render() {
     return (
       <div>
-        <div>This is the first component</div>.
+        <div>This is the first component</div>
+        <h2>Hello people</h2>
       </div>
     )
   }
